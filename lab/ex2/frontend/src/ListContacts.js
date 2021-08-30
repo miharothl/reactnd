34ab78f7d0273ede505361420935d109d8a6c0e6
Miha <miha@rothl.com>
2021-08-30 16:27:30 +0100

    implement ex2

---
+++ lab/ex2/frontend/src/ListContacts.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+
+class ListContacts extends Component {
+    render() {
+
+        console.log('Props', this.props)
+        return (
+            <ol className='contact-list'>
+                {this.props.contacts.map(contact => (
+                    <li key={contact.id} className='contact-list-item'>
+                        <div
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}
+                        ></div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <div className='contact-remove'>
+                            Remove
+                        </div>
+                    </li>
+                    ))}
+            </ol>
+        );
+    }
+}
+
+export default ListContacts
