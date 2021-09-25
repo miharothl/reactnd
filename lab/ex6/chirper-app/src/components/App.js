afddea6e92f1c7d8f4f4c66ee2dc490f2b585bf9
Miha <miha@rothl.com>
2021-09-25 17:07:07 +0100

    add Dashboard component

--- lab/ex6/chirper-app/src/components/App.js
+++ lab/ex6/chirper-app/src/components/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
+import Dashboard from "./Dashboard";
 
 class App extends Component {
 
@@ -12,10 +13,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        {
+          this.props.loading === true
+            ? null
+            : <Dashboard/>
+        }
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps({authedUser}) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
